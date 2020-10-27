d811776bfc276f83a26cbbd4adf1c9eb3dea0399
王帝 <di.wang@eoitek.com>
2020-10-27 16:15:02 +0800

    增强CLI：支持kerberos认证，新增参数principal，keytab

--- marayarn-cli/README.md
+++ marayarn-cli/README.md
@@ -13,40 +13,43 @@ The command line can be used to:
 
 #### submit application
 ```
-bin/marayarn submit \
+./marayarn submit \
 -cpu 1 -memory 512 -name hello_world -instance 2 \
 -cmd 'while true; do date; sleep 5; done'
 ```
 
 #### kill application
 ```
-bin/marayarn kill -app <appID>
+./marayarn kill -app <appID>
 ```
 
 #### status application
 ```
-bin/marayarn status -app <appID>
+./marayarn status -app <appID>
 ```
 
 #### info application
 ```
-bin/marayarn info -app <appID>
+./marayarn info -app <appID>
 ```
 
 #### scale application
 ```
-bin/marayarn scale -app <appID> -instance 3
+./marayarn scale -app <appID> -instance 3
 ```
 
 ## Usage
 
 The command line syntax is as follows:
 
-bin/marayarn <ACTION> [OPTIONS]
+./marayarn <ACTION> [OPTIONS]
 
 The following actions are available:
+
 - submit
 - kill
 - status
 - info
 - scale
+
+Use `-h` to get more information
--- marayarn-cli/src/main/java/com/eoi/marayarn/InfoOptions.java
+++ marayarn-cli/src/main/java/com/eoi/marayarn/InfoOptions.java
@@ -8,7 +8,13 @@ public class InfoOptions extends CliOptions  {
         Options options = new Options();
         Option app = new OptionBuilder("app").hasArg(true).argName("app").required()
                 .desc("Set application id of the Application").build();
+        Option principal = new OptionBuilder("principal").hasArg(true)
+                .desc("Principal to be used to login to KDC, while running secure HDFS").build();
+        Option keytab = new OptionBuilder("keytab").hasArg(true)
+                .desc("The full path to the file that contains the keytab for the principal specified above").build();
         options.addOption(app);
+        options.addOption(principal);
+        options.addOption(keytab);
         return options;
     }
 
@@ -20,8 +26,10 @@ public class InfoOptions extends CliOptions  {
         checkArguments(commandLine);
 
         ClientArguments clientArguments = new ClientArguments();
-        String id = commandLine.getOptionValue("app");
-        clientArguments.setApplicationId(id);
+        String app = commandLine.getOptionValue("app");
+        clientArguments.setApplicationId(app);
+        clientArguments.setPrincipal(commandLine.getOptionValue("principal", null));
+        clientArguments.setKeytab(commandLine.getOptionValue("keytab", null));
         return clientArguments;
     }
 
