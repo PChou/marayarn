95419ee334c5659bd793000a0db20a91cd581c82
周平 <parker.zhou@eoitek.com>
2022-07-27 15:38:31 +0800

    FIX EXECUTOR_LAUNCH_ENV_PREFIX avoid use .(dot)

--- marayarn-common/src/main/java/com/eoi/marayarn/Constants.java
+++ marayarn-common/src/main/java/com/eoi/marayarn/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
     public static final String EXECUTOR_ARTIFACTS_SIZES = "EXECUTOR_ARTIFACTS_SIZES";
     public static final String EXECUTOR_ARTIFACTS_VISIBILITIES = "EXECUTOR_ARTIFACTS_VISIBILITIES";
     public static final String EXECUTOR_ARTIFACTS_TYPES = "EXECUTOR_ARTIFACTS_TYPES";
-    public static final String EXECUTOR_LAUNCH_ENV_PREFIX = "EXECUTOR_LAUNCH_ENV_PREFIX.";
+    public static final String EXECUTOR_LAUNCH_ENV_PREFIX = "EXECUTOR_LAUNCH_ENV_PREFIX_";
 
 
     public static final String CONSTRAINTS_OPERATOR_CLUSTER = "CLUSTER";
